1c047b934f8d873ebba8f752be6c10f39f6b25e5
miguel-kjh <miguelangel.kjh@gmail.com>
2024-09-09 17:21:59 +0100

    add requirements

--- llm.py
+++ llm.py
@@ -14,3 +14,10 @@ import sentencepiece as spm
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
 torch.cuda.empty_cache()
+
+# main
+if __name__ == '__main__':
+    files_url = "https://ideami.com/llm_train"
+    print(f"Downloading files from {files_url}")
+    response = requests.get(files_url)
+    zipfile.ZipFile(io.BytesIO(response.content)).extractall(".")
