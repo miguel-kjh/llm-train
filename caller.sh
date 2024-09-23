b31c8e46147db28c3acf5c6b669fd77a50c8ce70
Miguel Angel Medina Ramirez <miguel-kjh>
2024-09-23 16:28:27 +0200

    add script

---
+++ caller.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --account=bsc81
+#SBATCH --job-name=test
+#SBATCH -q acc_bscls
+#SBATCH -N 1
+#SBATCH -t 02-00:00:00
+#SBATCH --mail-type=all
+#SBATCH --mail-user=miguel.medina@bsc.es
+#SBATCH --exclusive
+
+srun -n 1 -c 1 ./run.sh
